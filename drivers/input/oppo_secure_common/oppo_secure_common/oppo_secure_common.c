53e8f454644b02bb9eec26d024bc607526964197
XiKoTaSu_XKTS <3329989998@qq.com>
2026-07-11 13:30:24 +0800

    Add uaccess.h include for user access functions

--- drivers/input/oppo_secure_common/oppo_secure_common/oppo_secure_common.c
+++ drivers/input/oppo_secure_common/oppo_secure_common/oppo_secure_common.c
@@ -5,6 +5,7 @@
 
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 
 //#ifdef OPLUS_FEATURE_SECURITY_COMMON
 #if CONFIG_OPPO_BSP_SECCOM_PLATFORM == 6763 || CONFIG_OPPO_BSP_SECCOM_PLATFORM == 6771 || CONFIG_OPPO_BSP_SECCOM_PLATFORM == 6885 \
