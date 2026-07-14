6c43867e6d19b2429522a0c9ca94cb33a8327c42
XiKoTaSu <3329989998@qq.com>
2026-07-14 15:02:22 +0800

    kernel: Add initial support for droidian

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -167,7 +167,7 @@ void inotify_show_fdinfo(struct seq_file *m, struct file *f)
 
 #ifdef CONFIG_FANOTIFY
 
-static void fanotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
+static void fanotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark, struct file *f)
 {
 	unsigned int mflags = 0;
 	struct inode *inode;
